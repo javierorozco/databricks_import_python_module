9185d6848224f40245d7ab194f5d4010e35c45af
Javier <harvybcn@hotmail.com>
2019-07-18 22:41:29 +0100

    Change example functions

--- test.py
+++ test.py
@@ -1,12 +1,41 @@
-import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
-def create_df():
-    df = pd.DataFrame({ 'length': [1.5, 0.5, 1.2, 0.9, 3],
-                        'width': [0.7, 0.2, 0.15, 0.2, 1.1]
-                        }, index= ['pig', 'rabbit', 'duck', 'chicken', 'horse']
-                      )
-    return df
 
-def plot_pandas_df(df):
-    hist = df.hist(bins=3)
-    return hist
+def create_data():
+    x = np.linspace(0, 2 * np.pi, 50)
+    y = np.sin(x)
+    y2 = y + 0.1 * np.random.normal(size=x.shape)
+    return x, y, y2
+
+
+def create_plot(x, y, y2):
+    fig, ax = plt.subplots()
+    ax.plot(x, y, 'g--')
+    ax.plot(x, y2, 'ro')
+
+    # set ticks and tick labels
+    ax.set_xlim((0, 2 * np.pi))
+    ax.set_xticks([0, np.pi, 2 * np.pi])
+    ax.set_xticklabels(['0', '$\pi$', '2$\pi$'])
+    ax.set_ylim((-1.5, 1.5))
+    ax.set_yticks([-1, 0, 1])
+
+    # Only draw spine between the y-ticks
+    ax.spines['left'].set_bounds(-1, 1)
+
+    # Hide the right and top spines
+    ax.spines['right'].set_visible(False)
+    ax.spines['top'].set_visible(False)
+    # Only show ticks on the left and bottom spines
+    ax.yaxis.set_ticks_position('left')
+    ax.xaxis.set_ticks_position('bottom')
+
+    return fig
+
+
+
+
+
+
+
